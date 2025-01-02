910ef378c9711e97534891ffc968f3a3989b0c0d
mohammadreza <mohammadreza.fakhari84@gmail.com>
2025-01-02 03:36:23 -0800

    Update WallWizard.py

--- WallWizard.py
+++ WallWizard.py
@@ -42,4 +42,57 @@ def movement_choice():
                 wall_placement(wall_location1,wall_location2)
         except: 
             print("Cordinates should be in form of (x, y) like 0 2.")
-            return movement_choice()
+            return movement_choice()turn = 0
+
+players = [
+    Player(YELLOW, (4, 0)),
+    Player(WHITE, (4, 8))
+]
+from pygame.locals import (
+    K_w,
+    K_d,
+    K_a,
+    K_s,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+    MOUSEBUTTONDOWN
+)
+running = True
+while running:
+    screen.fill(BROWN)
+
+    for row in range(rows):
+        for column in range(columns):
+            pygame.draw.rect(screen, BLUE, [column * cell_size, row * cell_size, cell_size, cell_size], 5)
+
+    for player in players:
+        player.draw(screen)
+
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type==pygame.MOUSEBUTTONDOWN:
+                x_mouse,y_mouse=pygame.mouse.get_pos()
+                y=y_mouse//cell_size
+                x=x_mouse//cell_size
+                players[turn].move(x,y)
+                turn=1-turn
+        if event.type == pygame.KEYDOWN:
+            x,y=players[turn].position
+            if event.key == pygame.K_w: 
+                players[turn].move(x,y-1)
+                turn=1-turn
+            if event.key == pygame.K_s:
+                players[turn].move(x,y+1)
+                turn=1-turn
+            if event.key == pygame.K_a:
+                players[turn].move(x-1,y)
+                turn=1-turn
+            if event.key == pygame.K_d: 
+                players[turn].move(x+1,y)
+                turn=1-turn
+pygame.quit()
+sys.exit()
