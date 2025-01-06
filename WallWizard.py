ce5080b37f53501cedb1f6c8da17947580819218
Mohammadreza <mohammadreza.fakhari84@gmail.com>
2025-01-05 16:34:00 -0800

    Merging Karen and Negar code to main
    Add Jumping, fix placing walls outside of board, fix placing walls on each other.

--- WallWizard.py
+++ WallWizard.py
@@ -3,17 +3,19 @@ import sys
 from pygame.locals import (
     K_w, K_d, K_a, K_s, K_ESCAPE, KEYDOWN, QUIT, MOUSEBUTTONDOWN
 )
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, color, position):
         super(Player, self).__init__()
         self.color = color
         self.radius = 15
         self.position = position
+
     def move(self, new_x, new_y):
         if (0 <= new_x < columns and 0 <= new_y < rows) and (
-            (abs(new_x-self.position[0]) == 1 and abs(new_y-self.position[1]) == 0) or 
-            (abs(new_y-self.position[1]) == 1 and abs(new_x-self.position[0]) == 0)):
-            
+            (abs(new_x-self.position[0]) == 1 and abs(new_y-self.position[1]) == 0) or
+                (abs(new_y-self.position[1]) == 1 and abs(new_x-self.position[0]) == 0)):
+
             if players[0].position == players[1].position:
                 if players[0].position[0] == players[1].position[0]:
                     players[turn].move(new_x, new_y + 1)
@@ -22,10 +24,12 @@ class Player(pygame.sprite.Sprite):
             self.position = (new_x, new_y)
             return True
         return False
+
     def draw(self, surface):
         x = self.position[0] * cell_size + cell_size // 2
         y = self.position[1] * cell_size + cell_size // 2
         pygame.draw.circle(surface, self.color, (x, y), self.radius)
+
 pygame.init()
 screen_width = 500
 screen_height = 500
@@ -44,79 +48,167 @@ players = [
 ]
 walls = []
 centercells = []
-def wall(orientation, wall_start, wall_end,centercell):
+
+def wall(orientation, wall_start, wall_end, centercell):
     if (wall_start, wall_end) in walls or (wall_end, wall_start) in walls:
         return False
     if centercell in centercells:
         return False
+    y,x= wall_end
+    if not within_bounds(x,y):
+        return False
+    # بررسی همپوشانی نکردن دیوار ها
+    for existing_wall_start, existing_wall_end in walls:
+        if orientation == "H" and existing_wall_start[0] == wall_start[0]:
+            if (wall_start[1] <= existing_wall_end[1] and wall_end[1] >= existing_wall_start[1]):
+                return False
+
+        if orientation == "V" and existing_wall_start[1] == wall_start[1]:
+            if (wall_start[0] <= existing_wall_end[0] and wall_end[0] >= existing_wall_start[0]):
+                return False
     centercells.append(centercell)
     walls.append((wall_start, wall_end))
+    return True
+def within_bounds(x, y):
+        return 0 <= x < 9 and 0 <= y <9
 
+def adjacent_move( x, y,new_x, new_y ):
+        return abs(new_x - x) + abs(new_y - y) == 1
+
+def is_valid_move(player, new_pos):
+    
+    x, y = players[player].position
+    new_x, new_y = new_pos
+    if not within_bounds(new_x, new_y ):
+        return False
+    if not adjacent_move( x, y, new_x, new_y ):
+        return False
+ #   if blocked_by_wall(x, y, new_x, new_y ):
+  #      return False
+    return True
+def jump (player, new_pos):
+    x, y = players[player].position
+    new_x, new_y = new_pos
+    if not is_valid_move(player, new_pos):
+        return False
+        
+    opp = 1 - player 
+    opp_x, opp_y = players[opp].position
+    if new_pos == (opp_x, opp_y): 
+        dx = opp_x - x
+        dy = opp_y - y
+        jump_x = opp_x + dx
+        jump_y = opp_y + dy
+       
+        if not within_bounds(jump_x, jump_y ):
+            return False
+      #  if blocked_by_wall(x, y, jump_x, jump_y ):
+       #   return False
+        else:
+            players[player].position = (jump_x, jump_y) 
+            return True
 running = True
 while running:
     screen.fill(BROWN)
+
     for row in range(rows):
         for column in range(columns):
             pygame.draw.rect(
                 screen, BLUE, [column * cell_size, row * cell_size, cell_size, cell_size], 5)
+                
     for wall_start, wall_end in walls:
         if wall_start[0] == wall_end[0]:
             origin_x = wall_start[1] * cell_size
             origin_y = wall_start[0] * cell_size
             pygame.draw.line(screen, WHITE, (origin_x, origin_y),
-                             (origin_x + cell_size*2, origin_y), 5)
+                             (origin_x + cell_size * 2, origin_y), 5)
         elif wall_start[1] == wall_end[1]:
             origin_x = wall_start[1] * cell_size
             origin_y = wall_start[0] * cell_size
             pygame.draw.line(screen, WHITE, (origin_x, origin_y),
-                             (origin_x, origin_y + 2*cell_size), 5)
+                             (origin_x, origin_y + 2 * cell_size), 5)
+
     for player in players:
         player.draw(screen)
+
     pygame.display.flip()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             x_mouse, y_mouse = pygame.mouse.get_pos()
+
             if y_mouse % cell_size < 5:
                 cell_y = y_mouse // cell_size
                 cell_x = x_mouse // cell_size
                 wall_type = "H"
                 wall_start = (cell_y, cell_x)
-                wall_end = (cell_y, cell_x+1)
-                centercell = (cell_y, cell_x+ 1)
+                wall_end = (cell_y, cell_x + 1)
+                centercell = (cell_y, cell_x + 1)
+                if wall("H", wall_start, wall_end, centercell):
+                    turn = 1 - turn  # دیوار که میذاره ، نوبت تغییر کنه
                 if not wall("H", wall_start, wall_end, centercell):
                     print('n')
-                wall("H", wall_start, wall_end, centercell)
             if x_mouse % cell_size < 5:
                 cell_y = y_mouse // cell_size
                 cell_x = x_mouse // cell_size
                 wall_type = "V"
                 wall_start = (cell_y, cell_x)
-                wall_end = (cell_y+1, cell_x)
-
+                wall_end = (cell_y + 1, cell_x)
                 centercell = (cell_y + 1, cell_x)
-                if not wall("V", wall_start, wall_end,centercell):
+                if wall("V", wall_start, wall_end, centercell):
+                    turn = 1 - turn  # دیوار که میذاره ، نوبت تغییر کنه
+                if not wall("V", wall_start, wall_end, centercell):
                     print('n')
-                wall("V", wall_start, wall_end,centercell)
+
             y = y_mouse // cell_size
             x = x_mouse // cell_size
-            if players[turn].move(x, y):
-                turn = 1 - turn
+            new_pos = (x,y)
+            if new_pos == (players[1-turn].position[0] , players[1-turn].position[1] ):
+                   jump(turn , new_pos ) 
+                   turn=1-turn
+            elif players[turn].move(x,y):
+                turn=1-turn
         if event.type == pygame.KEYDOWN:
-            x, y = players[turn].position
-            if event.key == pygame.K_w:
-                if players[turn].move(x, y - 1):
-                    turn = 1 - turn
+            x,y=players[turn].position
+            if event.key == pygame.K_w: 
+                new_pos = (players[turn].position[0],
+                          players[turn].position[1] - 1)
+                if new_pos == (players[1-turn].position[0] , players[1-turn].position[1] ):
+                   jump(turn , new_pos ) 
+                   turn=1-turn
+                elif is_valid_move(turn, new_pos):
+                   players[turn].position = new_pos
+                   turn=1-turn
             if event.key == pygame.K_s:
-                if players[turn].move(x, y + 1):
-                    turn = 1 - turn
+                new_pos = (players[turn].position[0],
+                          players[turn].position[1] + 1)
+                if new_pos == (players[1-turn].position[0] , players[1-turn].position[1] ):
+                   jump(turn , new_pos ) 
+                   turn=1-turn
+                elif is_valid_move(turn, new_pos):
+                   players[turn].position = new_pos
+                   turn=1-turn
+                
             if event.key == pygame.K_a:
-                if players[turn].move(x - 1, y):
-                    turn = 1 - turn
-            if event.key == pygame.K_d:
-                if players[turn].move(x + 1, y):
-                    turn = 1 - turn
+                new_pos = (players[turn].position[0] -1,
+                          players[turn].position[1] )
+                if new_pos == (players[1-turn].position[0] , players[1-turn].position[1] ):
+                   jump(turn , new_pos ) 
+                   turn=1-turn
+                elif is_valid_move(turn, new_pos):
+                   players[turn].position = new_pos
+                   turn=1-turn
+            if event.key == pygame.K_d: 
+                new_pos = (players[turn].position[0] + 1 ,
+                          players[turn].position[1] )
+                if new_pos == (players[1-turn].position[0] , players[1-turn].position[1] ):
+                   jump(turn , new_pos ) 
+                   turn=1-turn
+                elif is_valid_move(turn, new_pos):
+                   players[turn].position = new_pos
+                   turn=1-turn
 pygame.quit()
 sys.exit()
-
